72c738a1272996ed8c55661164fb347ebbf7a233
drew <drewdrewthis@gmail.com>
2019-10-20 16:31:58 +0200

    Get Object Detection to work

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
   console.log('Your document is ready!');
+
+  var utils = new Utils("errorMessage");
 
   utils.loadImageToCanvas('assets/ar-card-small.jpg', 'dstCanvasInput');
 
+  utils.printError('Loaded!');
   (function openCvReady() {
     cv['onRuntimeInitialized'] = () => {
       // https://docs.opencv.org/master/dd/d00/tutorial_js_video_display.html
 
       // GET VIDEO
       let video = document.getElementById("videoInput"); // video is the id of video tag
-      navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+        facingMode: {
+          exact: 'environment'
+        }
+      })
         .then(function (stream) {
           video.srcObject = stream;
           video.play();
@@ -18,30 +27,44 @@ document.addEventListener("DOMContentLoaded", function () {
           console.log("An error occurred! " + err);
         });
 
-      // // PUT TO CANVAS
-      // let canvasFrame = document.getElementById("canvasOutput"); // canvasOutput is the id of <canvas>
-      // let context = canvasFrame.getContext("2d");
-      // let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-      // let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-      // const FPS = 30;
+      // PUT TO CANVAS
+      let canvasFrame = document.getElementById("canvasOutput"); // canvasOutput is the id of <canvas>
+      let context = canvasFrame.getContext("2d");
+      let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+      let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+      const FPS = 30;
 
-      // function processVideo() {
-      //   const width = canvasFrame.width;
-      //   const height = canvasFrame.height;
-      //   let begin = Date.now();
-      //   context.drawImage(video, 0, 0, width, height);
-      //   src.data.set(context.getImageData(0, 0, width, height).data);
-      //   cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-      //   cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
-      //   // schedule next one.
-      //   let delay = 1000 / FPS - (Date.now() - begin);
-      //   setTimeout(processVideo, delay);
-      // }
+      function processVideo() {
+        const width = canvasFrame.width;
+        const height = canvasFrame.height;
+        let begin = Date.now();
+        context.drawImage(video, 0, 0, width, height);
+        src.data.set(context.getImageData(0, 0, width, height).data);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
+        // schedule next one.
+        let delay = 1000 / FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+      }
 
-      // // schedule first one.
-      // setTimeout(processVideo, 0);
+      // schedule first one.
+      setTimeout(processVideo, 0);
 
-      camShift(dst);
+      // CamShift Stuff
+
+      // Get Template Image Mat
+      let templateCanvas = document.getElementById('dstCanvasInput');
+      let templateCtx = templateCanvas.getContext("2d");
+      let templateImageData = templateCtx.getImageData(
+        0,
+        0,
+        templateCanvas.width,
+        templateCanvas.height
+      );
+
+      let templateMat = cv.matFromImageData(templateImageData);
+
+      // camShift(templateMat);
     };
   })();
 });
